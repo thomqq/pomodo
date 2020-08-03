f6f3d69f900176e42c799178df7e82a6656bced1
thomqq <thomqq@gmail.com>
2020-08-03 16:22:48 +0200

    Added prototype od render conter

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,18 +1,47 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 class Counter extends React.Component {
 
-    componentDidMount() {
-        let canvas = ReactDOM.findDOMNode(this.refs.counterCanvas);
-        let ctx = canvas.getContext("2d");
+    state = {
+        mins: '00',
+        secs: '00'
     }
 
+    constructor(props) {
+        super(props);
+        this.counterCanvasRef = React.createRef();
+      }
+
+    componentDidMount() {
+        this.renderCounter();
+    }
+
+    renderCounter() {
+        if( this.props.type === 'text') {
+            this.renderTextCounter();
+        } else {
+            this.renderShapeCounter();
+        }
+    }
+
+    renderTextCounter() {
+        let canvas = this.counterCanvasRef.current;
+        let ctx = canvas.getContext("2d");
+        ctx.font = '30px serif';
+        ctx.fillText(`${this.state.mins} : ${this.state.secs}`, 50, 90);
+    }
+
+    renderShapeCounter() {
+
+    }
+
+
+    
     render() {
         return (
             <div>
-                <canvas id='counterCanvas' ref='counterCanvas' />
-            </div>
+                <canvas ref={this.counterCanvasRef} />
+            </div> 
         )
     }
 }
